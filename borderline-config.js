0c54169363c896a1374d4a179184a65865d0f32b
Tezirg <jean.grizet16@imperial.ac.uk>
2017-03-17 07:12:00 +0000

    Fix plugin zip file post method with new plugin format. Also fixies configuration file usage

---
+++ borderline-config.js
@@ -0,0 +1,9 @@
+var config = {
+    port: 3000,
+    mongoUrl: 'mongodb://dev:borderline@211.152.59.220:27017/borderline',
+    pluginSourcesFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\sources',
+    pluginFileSystemFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\filesystem',
+    uiFolder: './node-modules/borderline-ui'
+};
+
+module.exports = config;
