d329bbe1782d62af466f82c5d417e567c00e99cf
Tezirg <jean.grizet16@imperial.ac.uk>
2017-03-27 11:00:23 +0100

    Moved CORS feature from developement to its own configuration flag

--- borderline-config.js
+++ borderline-config.js
@@ -3,7 +3,8 @@ var config = {
     mongoUrl: 'mongodb://dev:borderline@211.152.59.220:27017/borderline',
     pluginSourcesFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\sources',
     pluginFileSystemFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\filesystem',
-    uiFolder: './node-modules/borderline-ui'
+    uiFolder: './node-modules/borderline-ui',
+    enableCors: true
 };
 
 module.exports = config;
