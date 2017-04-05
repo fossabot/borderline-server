a6b33de0433188ecebba8632ae339b414bcf43a0
Tezirg <jean.grizet16@imperial.ac.uk>
2017-04-05 11:27:13 +0100

    Query replaced plugin to extension. Fixes Extension store

--- borderline-config.js
+++ borderline-config.js
@@ -1,8 +1,8 @@
 var config = {
     port: 3000,
     mongoUrl: 'mongodb://dev:borderline@211.152.59.220:27017/borderline',
-    pluginSourcesFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\sources',
-    pluginFileSystemFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\filesystem',
+    extensionSourcesFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\sources',
+    extensionFileSystemFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\filesystem',
     uiFolder: './node-modules/borderline-ui',
     enableCors: true
 };
