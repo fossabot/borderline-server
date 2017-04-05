b4dcd0f5d420165d5c5c945a85a73bc1e4d45612
Tezirg <jean.grizet16@imperial.ac.uk>
2017-04-05 11:33:57 +0100

    Switch back to local mongo instance, within imperial

--- borderline-config.js
+++ borderline-config.js
@@ -1,6 +1,6 @@
 var config = {
     port: 3000,
-    mongoUrl: 'mongodb://dev:borderline@211.152.59.220:27017/borderline',
+    mongoUrl: 'mongodb://jean:root@146.169.33.32:27020/borderline',
     extensionSourcesFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\sources',
     extensionFileSystemFolder: 'C:\\Users\\grizet_j\\imperial\\borderline-server\\.plugins\\filesystem',
     uiFolder: './node-modules/borderline-ui',
